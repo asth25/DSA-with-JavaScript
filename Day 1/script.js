45c99aa13f3d54329556db6860dacd9b956583e3
Aastha <m99557587@gmail.com>
2026-09-18 00:41:08 +0530

    Add JavaScript practice folders

---
+++ Day 1/script.js
@@ -0,0 +1,16 @@
+// 1 Print Numbers from 1 to N
+
+// for loop
+// let num = 5;
+// for(let i=1; i<=5; i++){
+//     console.log(i);
+    
+// }
+
+// while loop
+let num = 5;
+let i = 1;
+while(i<=num){
+    console.log(i);
+    i++;
+}
---
+++ Some basics question/script.js
@@ -0,0 +1,40 @@
+// …or push an existing repository from the command line
+// git remote add origin https://github.com/asth25/DSA-with-JavaScript.git
+// git branch -M main
+// git push -u origin main
+
+// Concatination
+
+// let a = "Aastha";
+// let b = "Singh";
+// console.log(a + " " + b);
+
+
+// let fname = "Mahi"
+// let lname = "Singhania"
+// console.log(`My name is ${fname} ${lname}.`);
+
+
+let prompt = require('promt-sync')()
+
+let age = Number(prompt('enter your age'))
+console.log(typeof age);
+
+console.log('age ${age}');
+
+
+// swap 
+
+let a = 10;
+let b = 20;
+console.log(a);
+console.log(b);
+
+let temp = a;
+a=b;
+b=temp
+
+console.log(a);
+console.log(b);
+
+
