e2533a234d08cfc1336b80c1fad3b0e1ece70e50
Aastha <m99557587@gmail.com>
2026-09-18 11:08:03 +0530

    another question solved

--- Day 1/script.js
+++ Day 1/script.js
@@ -8,9 +8,25 @@
 // }
 
 // while loop
-let num = 5;
-let i = 1;
-while(i<=num){
-    console.log(i);
-    i++;
-}
+// let num = 5;
+// let i = 1;
+// while(i<=num){
+//     console.log(i);
+//     i++;
+// }
+
+// 2 Print Numbers from N to 1 without changing the loop condition of above question
+
+// for loop
+// let num = 5;
+// for(let i=1; i<=num; i++){
+//     console.log(num - i + 1);  
+// }
+
+// while loop
+// let num = 5;
+// let i = 1;
+// while(i<=num){
+//     console.log(num - i + 1);
+//     i++;
+// }
