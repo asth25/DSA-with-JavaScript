b856f20d74a6faa6e4a194c540f7ffb8eba628dc
Aastha <m99557587@gmail.com>
2026-09-18 21:28:15 +0530

    all questions done

--- Day 1/script.js
+++ Day 1/script.js
@@ -4,7 +4,7 @@
 // let num = 5;
 // for(let i=1; i<=5; i++){
 //     console.log(i);
-    
+
 // }
 
 // while loop
@@ -20,7 +20,7 @@
 // for loop
 // let num = 5;
 // for(let i=1; i<=num; i++){
-//     console.log(num - i + 1);  
+//     console.log(num - i + 1);
 // }
 
 // while loop
@@ -30,3 +30,111 @@
 //     console.log(num - i + 1);
 //     i++;
 // }
+
+// 3 Print All Even Numbers from 1 to N
+
+// for loop
+// let a = 10;
+// for (let i = 1; i <= a; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// while loop
+// let b = 10;
+// let i = 1;
+// while (i <= b) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+//   i++;
+// }
+
+// 4 Sum of First N Natural Numbers
+
+// for loop
+// let n=5;
+// let sum = 0;
+// for(let i = 1; i<=n;i++){
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+// while loop
+// let m=5;
+// let sum = 0;
+// let i = 1
+// while(i<=m){
+//     sum = sum + i;
+//     i++;
+// }
+// console.log(sum);
+
+// 5 Product (Factorial) of N
+
+// for loop
+// let n = 5;
+// let fractional = 1;
+// for (let i = 1; i <= n; i++) {
+//   fractional = fractional * i;
+// }
+// console.log(fractional);
+
+// while loop
+// let N = 5;
+// let product = 1;
+// let i = 1;
+
+// while (i <= N) {
+//     product = product * i;
+//     i++;
+// }
+
+// console.log(product);
+
+// 6 Sum of All Even Numbers up to N
+
+// for loop
+// let n =  10;
+// let sum = 0;
+// for(let i=1; i<=n;i++){
+//     if(i%2===0){
+//         sum = sum + i;
+//     }
+// }
+// console.log(sum);
+
+// while loop
+// let n = 10;
+// let sum = 0;
+// let i = 1;
+// while (i <= n) {
+//   if (i % 2 === 0) {
+//     sum = sum + 1;
+//   }
+//   i++;
+// }
+// console.log(sum);
+
+// 7 Print Squares of Numbers from 1 to N
+
+// for loop
+// let n = 5;
+// let squ = 0;
+// for(let i = 1; i<=n;i++){
+//     squ = i*2
+//     console.log(squ);
+// }
+
+// while loop
+// let n=5;
+// let squ = 0;
+// let i = 1;
+// while(i<=n){
+//     squ = i*i;
+//     console.log(squ);
+//     i++
+// }
+
+
