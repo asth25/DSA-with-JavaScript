a2bc2feef0baa4cf9284dd6d0d1d2d642dcbdb6a
Aastha <m99557587@gmail.com>
2026-09-19 20:28:54 +0530

    Day 2 ques 1

---
+++ Day 2/script.js
@@ -0,0 +1,17 @@
+// 🧩 Core Concept Focus
+// Conditional statements (if, else if, else)
+// Comparison and logical operators
+
+
+// 1 Find the Maximum of Three Numbers
+
+let n = [10,20,5]
+if(n[0]>=n[1] && n[0]>=n[2]){
+console.log(n[0]);
+}
+else if (n[1]>=n[0] && n[1]>=n[2]) {
+    console.log(n[1]);   
+}
+else{
+    console.log(n[2]);
+}
