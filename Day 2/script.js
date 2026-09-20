f01e8bdb39ef1eeb808620fab6785820d086d1c2
Aastha <m99557587@gmail.com>
2026-09-20 20:52:29 +0530

    change variable name

--- Day 2/script.js
+++ Day 2/script.js
@@ -15,10 +15,10 @@ if (n[0] >= n[1] && n[0] >= n[2]) {
 
 // 2 Check if a Number is Positive, Negative, or Zero
 
-let n = -9;
-if (n > 0) {
+let a = -9;
+if (a > 0) {
   console.log("Positive");
-} else if (n < 0) {
+} else if (a < 0) {
   console.log("Nagetive");
 } else {
   console.log("Null");
