1c355f835a2b1713b5fd5967b724ac9010d93e7c
Aastha <m99557587@gmail.com>
2026-09-20 20:00:51 +0530

    another ques in fever

--- Day 2/script.js
+++ Day 2/script.js
@@ -5,13 +5,26 @@
 
 // 1 Find the Maximum of Three Numbers
 
-let n = [10,20,5]
-if(n[0]>=n[1] && n[0]>=n[2]){
-console.log(n[0]);
+// let n = [10,20,5]
+// if(n[0]>=n[1] && n[0]>=n[2]){
+// console.log(n[0]);
+// }
+// else if (n[1]>=n[0] && n[1]>=n[2]) {
+//     console.log(n[1]);   
+// }
+// else{
+//     console.log(n[2]);
+// }
+
+// 2 Check if a Number is Positive, Negative, or Zero
+
+let n = -9;
+if(n>0){
+    console.log("Positive");
 }
-else if (n[1]>=n[0] && n[1]>=n[2]) {
-    console.log(n[1]);   
+else if(n<0){
+    console.log("Nagetive");
 }
 else{
-    console.log(n[2]);
+    console.log("Null");
 }
